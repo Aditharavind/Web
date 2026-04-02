ea1bfb09b88ea1acdb31ee68278bafa16d4ee51a
Aditharavind <45088223+Aditharavind@users.noreply.github.com>
2026-04-02 06:13:55 -0600

    updated postgres

--- app/database/db.py
+++ app/database/db.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://hhha@localhost/nawaglobal")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://nawa_global_user:cpkSm5wj203w0hgohqDGZ8DmSTMj7got@dpg-d775pc7afjfc73d9elo0-a/nawa_global")
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
